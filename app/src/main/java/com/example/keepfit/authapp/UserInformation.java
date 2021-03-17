0bbf81d280a60ab9036d798b7c2f2d605028a347
kaitlynyang <yangkait@usc.edu>
2021-03-17 12:24:11 -0700

    WORKING

--- app/src/main/java/com/example/keepfit/authapp/UserInformation.java
+++ app/src/main/java/com/example/keepfit/authapp/UserInformation.java
@@ -1,18 +1,17 @@
 package com.example.keepfit.authapp;
 
-import com.google.firebase.storage.StorageReference;
 
 public class UserInformation {
 
-    public String email, username, phonenumber, gender, birthday, weight, height, pickey;
+    public String email, name, phonenumber, gender, birthday, weight, height, pickey;
 
     public UserInformation(){
 
     }
 
-    public UserInformation(String email, String username, String phonenumber, String gender, String birthday, String weight, String height, String pickey){
+    public UserInformation(String email, String name, String phonenumber, String gender, String birthday, String weight, String height, String pickey){
         this.email = email;
-        this.username = username;
+        this.name = name;
         this.phonenumber = phonenumber;
         this.gender = gender;
         this.birthday = birthday;
