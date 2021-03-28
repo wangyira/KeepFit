288ae63f7132414771c12e7b7d608883550068ac
kaitlynyang <yangkait@usc.edu>
2021-03-28 13:52:26 -0700

    default avatar

--- app/src/main/java/com/example/keepfit/authapp/UserInformation.java
+++ app/src/main/java/com/example/keepfit/authapp/UserInformation.java
@@ -12,6 +12,14 @@ public class UserInformation {
     }
 
     public UserInformation(String email, String name, String phonenumber, String gender, String birthday, String weight, String height, String pickey, String username, String referenceTitle){
+        if (pickey == null) {
+            this.pickey = "https://firebasestorage.googleapis.com/v0/b/keepfit-99e86.appspot.com/o/profilepictures%2Fdefavatar.png?alt=media&token=6e7fd7b9-db1d-49b9-91a8-57c86a79a7df";
+            this.referenceTitle = "defavatar.png";
+        }
+        else {
+            this.pickey = pickey;
+            this.referenceTitle = referenceTitle;
+        }
         this.email = email;
         this.name = name;
         this.phonenumber = phonenumber;
@@ -19,8 +27,8 @@ public class UserInformation {
         this.birthday = birthday;
         this.weight = weight;
         this.height = height;
-        this.pickey = pickey;
+        //this.pickey = pickey;
         this.username = username;
-        this.referenceTitle = referenceTitle;
+        //this.referenceTitle = referenceTitle;
     }
 }
